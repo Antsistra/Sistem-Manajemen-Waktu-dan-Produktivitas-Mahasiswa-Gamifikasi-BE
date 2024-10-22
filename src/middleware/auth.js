b5e3d143c296addcc5d051d2367d1d1dd0feb11c
Antsistra <hadinurmuhamad8@gmail.com>
2024-10-22 09:10:25 +0700

    Start Create Project

---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/jwt");
+const AuthToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  jwt.verify(token, config.jwtSecret, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = AuthToken;
